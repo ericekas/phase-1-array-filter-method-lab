f1418bf482291034bce9cd0735b9fdfd5b671395
Eric Ekas <eric.ekas@gmail.com>
2022-05-18 13:42:25 -0400

    commit tree 656905b112df80a79e052d878cf4fbfc3ac00ef9

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter((element) => element.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter((element) => element.slice(0,name.length) === name)
+}
+
+function matchName(drivers, checkName) {
+    return drivers.filter((element) => element.name === checkName)
+}
+
+
